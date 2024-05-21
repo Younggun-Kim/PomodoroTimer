cc5ecea961d542e5b5e1d2f88bbba9d7be976357
gim-yeong-geon <dudrjs145@naver.com>
2024-05-21 16:06:01 +0900

    타이머 시작, 일시정지 추가.

--- PomodoroTimer/UI/Home/HomeReactor.swift
+++ PomodoroTimer/UI/Home/HomeReactor.swift
@@ -26,12 +26,55 @@ class HomeReactor: Reactor, Stepper {
     // MARK: Reactor
     
     enum Action {
-        
+        case play
+        case pause
+    }
+    
+    enum Mutation {
+        case setTime(Int)
+        case setRunning(Bool)
     }
     
     struct State {
+        var currentTime: Int = 0
         
+        var isRunning: Bool = false
     }
     
     var initialState = State()
+    
+    var timer = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .play:
+            return Observable.concat([
+                Observable.just(Mutation.setRunning(true)),
+                self.startTimer()
+            ])
+        case .pause:
+            return Observable.just(Mutation.setRunning(false))
+            
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        
+        switch mutation {
+        case let .setTime(time):
+            state.currentTime = time
+        case let .setRunning(isRunning):
+            state.isRunning = isRunning
+        }
+        
+        return state
+    }
+    
+    private func startTimer() -> Observable<Mutation> {
+        return Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .take(until: self.action.filter({ $0 == .pause }))
+            .take(while: { _ in self.currentState.currentTime < 5})
+            .map { _ in Mutation.setTime(self.currentState.currentTime + 1)}
+    }
 }
--- PomodoroTimer/UI/Home/HomeViewController.swift
+++ PomodoroTimer/UI/Home/HomeViewController.swift
@@ -8,17 +8,36 @@
 import UIKit
 import Reusable
 import RxSwift
+import RxCocoa
 import ReactorKit
+import HGCircularSlider
+import Then
 
 class HomeViewController: UIViewController, ReactorBased, StoryboardBased {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var circularSlider: CircularSlider!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    
+    // MARK: - ReactorKit
+    
     typealias Reactor = HomeReactor
     
     var disposeBag = DisposeBag()
     
-    func bind(reactor: HomeReactor) {
-        self.bindAction(reactor: reactor)
-        self.bindState(reactor: reactor)
-    }
+    // MARK: - View
+    
+    // date formatter user for timer label
+    let dateComponentsFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.minute, .second]
+        
+        return formatter
+    }()
     
     init(reactor: HomeReactor!) {
         super.init(nibName: nil, bundle: nil)
@@ -31,16 +50,89 @@ class HomeViewController: UIViewController, ReactorBased, StoryboardBased {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configView()
+        
+        if let reactor = self.reactor {
+            self.bind(reactor: reactor)
+        }
+    }
+    
+    private func configView() {
+        self.circularSlider.do {
+            $0.minimumValue = 0.0
+            $0.maximumValue = 60.0
+            $0.endPointValue = 30
+            $0.diskFillColor = .red
+            $0.diskColor = .white
+            $0.lineWidth = 12
+            $0.backtrackLineWidth = 12
+            $0.trackFillColor = .green
+            $0.trackColor = .gray.withAlphaComponent(0.3)
+            
+            $0.isUserInteractionEnabled = false
+            $0.thumbLineWidth = 0.0
+            $0.thumbRadius = 0.0
+        }
+        
+        self.timeLabel.do {
+            var components = DateComponents()
+            components.second = Int(30)
+            $0.text = dateComponentsFormatter.string(from: components)
+            $0.font = .systemFont(ofSize: 30, weight: .bold)
+        }
+    }
+    
+    // MARK: Bind Reactor
+    func bind(reactor: HomeReactor) {
+        self.bindAction(reactor: reactor)
+        self.bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: HomeReactor) {
+        playBtn?
+            .rx
+            .tap
+            .map { HomeReactor.Action.play }
+            .debug()
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        pauseBtn?
+            .rx
+            .tap
+            .map { HomeReactor.Action.pause}
+            .debug()
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+    }
+    
+    func bindState(reactor: HomeReactor) {
+        
+        guard let slider = circularSlider else {
+            return
+        }
+        
+        reactor
+            .state
+            .compactMap { $0.currentTime }
+            .map { CGFloat($0) }
+            .bind(to: circularSlider.rx.endPointValue)
+            .disposed(by: self.disposeBag)
+        
+        reactor
+            .state
+            .map { $0.currentTime }
+            .compactMap { [weak self] (currentTime: Int) in
+                var components = DateComponents()
+                components.second = currentTime
+                return self?.dateComponentsFormatter.string(from: components)
+            }
+            .bind(to: timeLabel.rx.text)
+            .disposed(by: self.disposeBag)
     }
 }
 
 extension HomeViewController {
     
-    func bindAction(reactor: HomeReactor) {
-        
-    }
-    
-    func bindState(reactor: HomeReactor) {
-        
-    }
 }
